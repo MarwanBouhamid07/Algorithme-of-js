a5c57c93bca6460902d747f3dfa39954cfc312f0
marwan <marwanbouhamid123@gmail.com>
2026-09-08 09:45:05 +0100

    make some improvement in the code

---
+++ exchange two varaibles/app.js
@@ -0,0 +1,16 @@
+let a = 5; //varaible 1
+
+console.log("value of varaile num 1 before exchange it :", a);
+
+let b = 6; //varaible 2
+
+console.log("value of varaile num 2 before exchange it :", b);
+
+let c = 0; //varaible 3 for save the value like memory
+
+c = a ; // c = 5
+a = b ; // a = 6
+b = c ; //b = 5 
+
+console.log("value of varaile num 1 after exchange it :", a);
+console.log("value of varaile num 2 after exchange it :", b);
