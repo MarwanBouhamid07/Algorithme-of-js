a74c04169c688688f87123b187ed527c56288cbc
marwan <marwanbouhamid123@gmail.com>
2026-09-09 15:21:26 +0100

    find the the duplicate number from the table

--- proccesing an array/app.js
+++ proccesing an array/app.js
@@ -1,10 +1,18 @@
-let array = [1, 2, 3, 2, 4, 5, 1, 6, 2];
+let array = [1, 2, 3, 2, 4, 5, 6, 2];
 let duplicates = [];
+let count;
 
-for (let value of array) {
-    if (array.indexOf(value) !== array.lastIndexOf(value) && !duplicates.includes(value)) {
-        duplicates.push(value);
+for (let index = 0; index < array.length; index++) {
+    let def = array[index];
+    count = 0;
+    for (let i = 0; i < array.length; i++) {
+        if(array[i + index] === def){
+                duplicates[duplicates.length] = array[i + index]
+        }
+        
     }
+
 }
 
-console.log(duplicates); // [1, 2]
+console.log(duplicates);
+
